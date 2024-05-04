354ee612c96a7b7b03532e4a4f0fe45d52af2ea8
Arumi <arumi@thwiki.cc>
2024-05-04 19:52:21 +0800

    Optimized

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,3 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
