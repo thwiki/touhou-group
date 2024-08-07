7602126cd065569dc90c0272db8443dcf19cc161
Arumi <26451498+arumi-s@users.noreply.github.com>
2024-08-07 11:12:54 +0800

    Updated image domains

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
 					'https://thwiki.cc/api.php?action=query&format=json&prop=revisions&list=&titles=%E4%B8%9C%E6%96%B9%E7%9B%B8%E5%85%B3QQ%E7%BE%A4%E7%BB%84%E5%88%97%E8%A1%A8&formatversion=2&rvprop=timestamp%7Cids',
 				parseApi:
 					'https://thwiki.cc/api.php?action=parse&format=json&page=%E4%B8%9C%E6%96%B9%E7%9B%B8%E5%85%B3QQ%E7%BE%A4%E7%BB%84%E5%88%97%E8%A1%A8&prop=text&wrapoutputclass=&disablelimitreport=1&disableeditsection=1&disabletoc=1&formatversion=2',
-				imageDomains: ['upload.thwiki.cc']
+				imageDomains: ['upload.thbwiki.cc']
 			}
 		}),
 		tailwind(),
